9b7281a7b99df0d55eb99fea1853f46c7f3fab84
박준영 <juny_0429@naver.com>
2023-10-24 01:48:02 +0900

    first commit

---
+++ src/main/java/com/green/cocktable/config/CocktableApplication.java
@@ -0,0 +1,13 @@
+package com.green.cocktable.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocktableApplication.class, args);
+    }
+
+}
